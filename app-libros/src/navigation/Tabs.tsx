6162e65f1f07a7a0c087c8d64f206d5739d5dffb
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-23 10:05:58 -0300

    build(frontend): push Tabs component

---
+++ app-libros/src/navigation/Tabs.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Tabs = () => {
+  return (
+    
+    <>
+    
+        <View>
+
+
+
+        </View>
+
+    </>
+
+  )
+}
+
+export default Tabs
