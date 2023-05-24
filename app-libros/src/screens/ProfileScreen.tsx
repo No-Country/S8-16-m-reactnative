615001a04c99b3317161df738a96c798170bf7d4
MatiasMass <matiasmass14@gmail.com>
2023-05-24 11:33:00 -0300

    feat(profile): Add expo-image and Style Profile screen

--- app-libros/src/screens/ProfileScreen.tsx
+++ app-libros/src/screens/ProfileScreen.tsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Image,
   Text,
   View,
   StyleSheet,
-  SafeAreaViewComponent,
+  TouchableHighlight,
+  Button,
+  TouchableOpacity,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 export const ProfileScreen = () => {
+  const [image, setImage] = useState("");
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     // bg-bookBlack
     <ScrollView>
@@ -19,23 +40,32 @@ export const ProfileScreen = () => {
         <View className="flex justify-center items-center">
           <View className="relative">
             {/* Arreglar tamano de imagen */}
-            <Image source={require("../../assets/profile.png")} />
-            <View className="absolute right-3 -bottom-1 bg-bookYellow p-2 rounded-full">
-              <Ionicons name="md-pencil" size={28} color="black" />
-            </View>
+            {/* <Image source={require("../../assets/profile.png")} /> */}
+            <Image
+              source={
+                image ? { uri: image } : require("../../assets/profile.png")
+              }
+              style={{ width: 200, height: 200, borderRadius: 100 }}
+            />
+
+            {/* {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />} */}
+            <TouchableOpacity onPress={pickImage}>
+              <View className="absolute right-3 -bottom-1 bg-bookYellow p-2 rounded-full">
+                <Ionicons name="md-pencil" size={28} color="black" />
+              </View>
+            </TouchableOpacity>
           </View>
           <View className="flex justify-center items-center m-5">
             <Text className="text-xl text-bookWhite">Capiola</Text>
             <Text className="text-sm text-bookWhite">#Kapipiola</Text>
           </View>
         </View>
-        <View className="flex my-5" >
+        <View className="flex my-5">
           <Text className="text-bookWhite mb-10 pl-3 text-base">
             Libros publicados
           </Text>
           <ScrollView horizontal={true} className="flex">
-            <View className="flex flex-basis-0 gap-5 flex-row p-5 bg-bookDarkGrey" >
-                {/*  bg-bookDarkGrey */}
+            <View className="flex flex-1 gap-5 flex-row p-5 bg-bookDarkGrey">
               <View
                 className="flex  rounded-xl p-3 bg-bookGrey"
                 style={{ width: 150, height: 250 }}
@@ -47,28 +77,63 @@ export const ProfileScreen = () => {
                   <Feather name="plus" size={50} color="white" />
                 </View>
               </View>
+
               <View
-                className="flex  rounded-xl p-3 bg-bookGrey"
-                style={{ width: 150, height: 250 }}
+                className="flex flex-1 flex-row bg-[#171719] overflow-hidden rounded-xl"
+                style={{ width: 400 }}
               >
                 <View
-                  style={styles.bookContainer}
-                  className="flex border-dashed border-2 border-bookWhite justify-center items-center"
+                  className="flex justify-center items-center w-[30%]  p-3 bg-bookGrey"
+                  style={{ width: 150, height: 250 }}
                 >
-                  <Feather name="plus" size={50} color="white" />
+                  <Image
+                    className="object-fit"
+                    source={require("../../assets/profile.png")}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </View>
+                <View className="flex w-[70%] relative justify-center items-center ">
+                  <Text className="text-bookWhite text-sm">Titulo</Text>
+                  <View className="flex flex-row gap-2">
+                    <Text className="text-bookWhite text-sm">Autor</Text>
+                    <Text className="text-bookWhite text-sm">Año</Text>
+                  </View>
+                  <View className="absolute right-10 bottom-4 bg-bookYellow p-2 rounded-full">
+                    <Ionicons name="md-pencil" size={28} color="black" />
+                  </View>
                 </View>
               </View>
+
               <View
-                className="flex  rounded-xl p-3 bg-bookGrey"
-                style={{ width: 150, height: 250 }}
+                className="flex flex-1 flex-row bg-[#171719] overflow-hidden rounded-xl"
+                style={{ width: 400 }}
               >
                 <View
-                  style={styles.bookContainer}
-                  className="flex border-dashed border-2 border-bookWhite justify-center items-center"
+                  className="flex justify-center items-center w-[30%]  p-3 bg-bookGrey"
+                  style={{ width: 150, height: 250 }}
                 >
-                  <Feather name="plus" size={50} color="white" />
+                  <Image
+                    className="object-fit"
+                    source={require("../../assets/profile.png")}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </View>
+                <View className="flex w-[70%] relative justify-center items-center ">
+                  <Text className="text-bookWhite text-sm">Titulo</Text>
+                  <View className="flex flex-row gap-2">
+                    <Text className="text-bookWhite text-sm">Autor</Text>
+                    <Text className="text-bookWhite text-sm">Año</Text>
+                  </View>
+                  <View className="absolute right-10 bottom-4 bg-bookYellow p-2 rounded-full">
+                    <Ionicons name="md-pencil" size={28} color="black" />
+                  </View>
                 </View>
               </View>
+
+              {/*  */}
+
+
+
             </View>
           </ScrollView>
         </View>
@@ -78,7 +143,7 @@ export const ProfileScreen = () => {
           </Text>
           <View className="flex justify-center items-center gap-5 flex-1 flex-row flex-wrap">
             <View
-              className="flex p-2   rounded-2xl bg-bookGrey overflow-hidden justify-between"
+              className="flex p-2 rounded-2xl bg-bookGrey overflow-hidden justify-between"
               style={{ height: 125 }}
             >
               <View className="flex justify-center items-center">
