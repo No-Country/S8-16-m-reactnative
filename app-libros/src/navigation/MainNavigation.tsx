0852be341b92024c3aace035f4d07387b18ccdc7
Oscar Tandioy <tato.tandioy@gmail.com>
2023-05-23 22:25:39 -0500

    feat: Creation of the home screen of genres with styles and addition of frameworks

--- app-libros/src/navigation/MainNavigation.tsx
+++ app-libros/src/navigation/MainNavigation.tsx
@@ -1,22 +1,28 @@
-import { createStackNavigator } from '@react-navigation/stack'
-import { HomeScreen } from '../screens/HomeScreen'
-import { LoginScreen } from '../screens/LoginScreen'
+import { StackView, createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/HomeScreen';
+import { LoginScreen } from '../screens/LoginScreen';
+import GenresBooks from '../components/GenresBooks/GenresBooks';
 
 export type RootStackParams = {
-    HomeScreen: undefined
-    LoginScreen: undefined
-}
-const Stack = createStackNavigator<RootStackParams>()
+  HomeScreen: undefined;
+  LoginScreen: undefined;
+  GenresBooks: undefined;
+};
+const Stack = createStackNavigator<RootStackParams>();
 
 export const MainNavigation = () => {
-    return (
-            <Stack.Navigator screenOptions={{
-                // headerShown: false
-            }}
-            initialRouteName='LoginScreen'
-            >
-                <Stack.Screen name='LoginScreen' component={LoginScreen} />
-                <Stack.Screen name='HomeScreen' component={HomeScreen} />
-            </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      screenOptions={
+        {
+          // headerShown: false
+        }
+      }
+      initialRouteName="GenresBooks"
+    >
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="GenresBooks" component={GenresBooks} />
+    </Stack.Navigator>
+  );
+};
