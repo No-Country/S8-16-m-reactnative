c3465cb7c16c0cc1a659c5ddeb4f5b14a678077d
eitan230 <eitan03@hotmail.com.ar>
2023-05-25 20:03:43 -0300

    create register components

--- app-libros/src/navigation/MainNavigation.tsx
+++ app-libros/src/navigation/MainNavigation.tsx
@@ -4,10 +4,16 @@ import { LoginScreen } from '../screens/LoginScreen';
 import { ChatScreen } from '../screens/ChatScreen';
 import { GenresBooks } from '../components/GenresBooks/GenresBooks';
 import { ProfileScreen } from '../screens/ProfileScreen'
+import { Register1 } from '../components/Register/Register1';
+import { Register2 } from '../components/Register/Register2';
+import { Register3 } from '../components/Register/Register3';
   
 export type RootStackParams = {
   HomeScreen: undefined;
   LoginScreen: undefined;
+  Register1: undefined;
+  Register2: undefined;
+  Register3: undefined;
   ChatScreen: undefined;
   GenresBooks: undefined;
   ProfileScreen: undefined
@@ -24,6 +30,11 @@ export const MainNavigation = () => {
       initialRouteName="LoginScreen"
     >
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Navigator>
+        <Stack.Screen name='Register1' component={Register1}/>
+        <Stack.Screen name='Register2' component={Register2}/>
+        <Stack.Screen name='Register3' component={Register3}/>
+      </Stack.Navigator>
       <Stack.Screen name="ChatScreen" component={ChatScreen} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="GenresBooks" component={GenresBooks} />
---
+++ app-libros/src/screens/RegisterScreen.tsx
@@ -0,0 +1,51 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Button, Text, TouchableOpacity, View, TextInput, Image, StyleSheet } from 'react-native';
+import { RootStackParams } from '../navigation/MainNavigation';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+
+export const RegisterScreen = () => {
+
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
+  return (
+    <View className='flex-1 justify-center items-center bg-[#1E1E1E]'>
+      <Image
+        source={require('../../assets/logo.png')}
+        style={{ width: 154, height: 69 }}
+      ></Image>
+      <Text
+        className='text-[#FFFFFF] text-base mb-7 mt-5'
+      >Ingresa tus datos</Text>
+      <TextInput
+        className='bg-[#272727] px-8 py-3 mb-6 w-4/5 rounded-xl text-xs'
+        placeholderTextColor="#4D4D4D"
+        placeholder="Ingresá tu nombre y apellido "
+      />
+      <TextInput
+        className='bg-[#272727] px-8 py-3 mb-7 w-4/5 rounded-xl text-xs '
+        placeholderTextColor="#4D4D4D"
+        placeholder="Ingresá tu nombre de usuario"
+      />
+      <TextInput
+        className='bg-[#272727] px-8 py-3 mb-7 w-4/5 rounded-xl text-xs '
+        placeholderTextColor="#4D4D4D"
+        placeholder="Ingresa tu contraseña"
+      />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('HomeScreen')}
+        activeOpacity={0.8}
+        className='bg-[#FF9A1F] px-8 py-4 mb-3 w-4/5 rounded-xl'
+      >
+        <Text className='text-[#171719] text-xs self-center'>
+          SIGUIENTE
+        </Text>
+
+      </TouchableOpacity>
+
+
+    </View>
+  )
+}
